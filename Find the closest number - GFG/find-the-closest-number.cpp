68522212df2307f118b663c89f9e64d13006530e
sanjeevWeb <108945506+sanjeevWeb@users.noreply.github.com>
2023-07-03 12:25:44 +0530

    Added solution - LeetHub

--- Find the closest number - GFG/find-the-closest-number.cpp
+++ Find the closest number - GFG/find-the-closest-number.cpp
@@ -10,15 +10,57 @@ class Solution{
     int findClosest(int arr[], int n, int target) 
     { 
         // Complete the function
-        int min = INT_MAX,diff,element;
-        for(int i=0;i<n;i++){
-            diff = abs(arr[i] - target);
-            if(diff <= min){
-                min = diff;
-                element = arr[i];
+        // int min = INT_MAX,diff,element;
+        // for(int i=0;i<n;i++){
+        //     diff = abs(arr[i] - target);
+        //     if(diff <= min){
+        //         min = diff;
+        //         element = arr[i];
+        //     }
+        // }
+        // return element;
+        
+        if(target < arr[0]){
+            return arr[0];
+        }
+        if(target > arr[n-1]) return arr[n-1];
+        
+        //---------apply BS for the rest----------
+        int left = 0;
+        int right = n-1;
+        while(left <= right){
+            int mid = left + (right - left)/2;
+            
+            if(arr[mid] == target){
+                 return arr[mid];
+            }
+            else if(arr[mid] > target){
+                if(arr[mid-1] <= target && arr[mid] > target){
+                    int diff1 = abs(arr[mid] - target);
+                    int diff2 = abs(arr[mid-1] - target);
+                    if(diff1 > diff2){
+                        return arr[mid-1];
+                    }
+                    else{
+                        return arr[mid];
+                    }
+                }
+                right = mid - 1;
+            }
+            else{
+                if(arr[mid] < target && arr[mid + 1] > target){
+                    int diff1 = abs(arr[mid] - target);
+                    int diff2 = abs(arr[mid+1] - target);
+                    if(diff1 >= diff2){
+                        return arr[mid+1];
+                    }
+                    else{
+                        return arr[mid];
+                    }
+                }
+                left = mid+1;
             }
         }
-        return element;
     } 
 };
 
