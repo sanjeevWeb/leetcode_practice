993841b4461c4578d74402b49ad04d3957134732
sanjeevWeb <108945506+sanjeevWeb@users.noreply.github.com>
2023-08-19 14:21:36 +0530

    Added solution - LeetHub

---
+++ Find the fine - GFG/find-the-fine.cpp
@@ -0,0 +1,54 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+
+// } Driver Code Ends
+
+
+class Solution{
+  public:
+    long long int totalFine(int n, int date, int car[], int fine[])
+    {
+        long long int totalFine = 0;
+        if(date%2 == 0){
+            for(int i=0;i<n;i++){
+                if(car[i]%2 != 0){
+                    totalFine += fine[i];
+                }
+            }
+        }
+        else{
+            for(int i=0;i<n;i++){
+                if(car[i]%2 == 0){
+                    totalFine += fine[i];
+                }
+            }
+        }
+        return totalFine;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n, i, date;
+        cin >> n >> date;
+        int car[n], fine[n];
+        
+        for (i = 0; i < n; i++){
+            cin >> car[i];
+        }
+        for (int i = 0; i < n; i++) {
+            cin >> fine[i];
+        }
+        Solution obj;
+        cout << obj.totalFine(n, date, car, fine) << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
