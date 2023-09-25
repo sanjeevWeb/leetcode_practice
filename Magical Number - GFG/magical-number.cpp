3e095eb68ae97dc3aece727ce61567c40ce0677f
sanjeevWeb <108945506+sanjeevWeb@users.noreply.github.com>
2023-09-25 20:45:48 +0530

    Added solution - LeetHub

--- Magical Number - GFG/magical-number.cpp
+++ Magical Number - GFG/magical-number.cpp
@@ -27,30 +27,18 @@ int main()
 int binarySearch(int arr[], int low, int high)
 {
       //add code here.
-      
-
-// int binarySearch(int arr[], int low, int high)
-// {
-//       //add code here.
-//       while(low<=high){
-//           int mid = low + (high - low)/2;
-//           if(arr[mid]==mid)return mid;
-//           else if(arr[mid]>mid)high = mid-1;
-//           else low=mid+1;
-//       }
-//       return -1;
-// }
       while(low<=high){
-          int mid = low+(high - low)/2;
+          int mid = low + (high-low)/2;
           if(arr[mid] == mid){
-              return arr[mid];
+              return mid;
           }
-          else if(arr[mid]>mid){
-              high = mid -1;
+          else if(arr[mid] > mid){
+              high = mid-1;
           }
           else{
-              low = mid + 1;
+              low = mid+1;
           }
       }
       return -1;
+      
 }
