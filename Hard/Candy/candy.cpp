f02e726f9c12a054af52819837424cbc8a8b75bc
sanjeevWeb <108945506+sanjeevWeb@users.noreply.github.com>
2023-12-21 21:29:08 +0530

    Added Solution - GfG to GitHub

---
+++ Hard/Candy/candy.cpp
@@ -0,0 +1,57 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution {
+  public:
+    int minCandy(int N, vector<int> &ratings) {
+        // code here
+        int n = ratings.size();
+        vector<int> candies(n, 1);  // Initialize all children with 1 candy
+    
+        // Traverse from left to right
+        for (int i = 1; i < n; ++i) {
+            if (ratings[i] > ratings[i - 1]) {
+                candies[i] = candies[i - 1] + 1;
+            }
+        }
+    
+        // Traverse from right to left
+        for (int i = n - 2; i >= 0; --i) {
+            if (ratings[i] > ratings[i + 1]) {
+                candies[i] = max(candies[i], candies[i + 1] + 1);
+            }
+        }
+    
+        // Calculate the total number of candies needed
+        int totalCandies = 0;
+        for (int i = 0; i < n; ++i) {
+            totalCandies += candies[i];
+        }
+    
+        return totalCandies;
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int N;
+        cin >> N;
+
+        vector<int> ratings(N);
+        for (int i = 0; i < N; i++) {
+            cin >> ratings[i];
+        }
+        Solution obj;
+        cout << obj.minCandy(N, ratings) << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
