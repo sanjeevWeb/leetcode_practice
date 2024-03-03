b86502ccdc22899b56b4a546f00e9216f43f94f1
sanjeevWeb <108945506+sanjeevWeb@users.noreply.github.com>
2024-03-03 13:03:28 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Find Indexes of a subarray with given sum/find-indexes-of-a-subarray-with-given-sum.cpp
@@ -0,0 +1,65 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution
+{
+    public:
+    //Function to find a continuous sub-array which adds up to a given number.
+    vector<int> subarraySum(vector<int>arr, int n, long long s)
+    {
+        int currentSum = 0;
+        int start = 0;
+    
+        for (int i = 0; i < n; i++) {
+            currentSum += arr[i];
+    
+            while (currentSum > s && start < i) {
+                currentSum -= arr[start];
+                start++;
+            }
+    
+            if (currentSum == s) {
+                return {start + 1, i + 1};  // 1-based indexing
+            }
+        }
+    
+        return {-1};  // No subarray found
+
+    }
+};
+
+
+
+//{ Driver Code Starts.
+
+int main()
+ {
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n;
+        long long s;
+        cin>>n>>s;
+        vector<int>arr(n);
+        // int arr[n];
+        const int mx = 1e9;
+        for(int i=0;i<n;i++)
+        {
+            cin>>arr[i];
+        }
+        Solution ob;
+        vector<int>res;
+        res = ob.subarraySum(arr, n, s);
+        
+        for(int i = 0;i<res.size();i++)
+            cout<<res[i]<<" ";
+        cout<<endl;
+        
+    }
+	return 0;
+}
+// } Driver Code Ends
