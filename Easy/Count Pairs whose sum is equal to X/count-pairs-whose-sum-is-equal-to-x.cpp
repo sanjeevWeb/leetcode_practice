e704baa78b3dc6d1074dd41cc0879c960144e034
sanjeevWeb <108945506+sanjeevWeb@users.noreply.github.com>
2024-03-17 18:46:31 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Count Pairs whose sum is equal to X/count-pairs-whose-sum-is-equal-to-x.cpp
@@ -0,0 +1,111 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node {
+    int data;
+    struct Node* next;
+
+    Node(int x) {
+        data = x;
+        next = NULL;
+    }
+};
+
+void append(struct Node** headRef, struct Node** tailRef, int newData) {
+    struct Node* new_node = new Node(newData);
+
+    if (*headRef == NULL) {
+        *headRef = new_node;
+    }
+
+    else {
+        (*tailRef)->next = new_node;
+    }
+    *tailRef = new_node;
+}
+
+void printList(struct Node* head) {
+    while (head) {
+        cout << head->data << ' ';
+        head = head->next;
+    }
+}
+
+
+// } Driver Code Ends
+/*
+Structure of the node of the linked list is as
+struct Node
+{
+    int data;
+    struct Node* next;
+
+    Node(int x){
+        data = x;
+        next = NULL;
+    }
+};
+*/
+class Solution{
+  public:
+    // your task is to complete this function
+    int countPairs(struct Node* head1, struct Node* head2, int x) {
+        // Code here
+        std::unordered_set<int> seen_values;
+
+    // Traverse the second linked list and add its values to the set
+        Node* ptr2 = head2;
+        while (ptr2) {
+            seen_values.insert(ptr2->data);
+            ptr2 = ptr2->next;
+        }
+    
+        // Initialize the count of valid pairs
+        int count = 0;
+    
+        // Traverse the first linked list
+        Node* ptr1 = head1;
+        while (ptr1) {
+            // Calculate the complement value needed for a valid pair
+            int complement = x - ptr1->data;
+    
+            // Check if the complement exists in the set
+            if (seen_values.count(complement)) {
+                count++;
+            }
+    
+            ptr1 = ptr1->next;
+        }
+    
+        return count;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int T;
+    cin >> T;
+    while (T--) {
+        struct Node* head1 = NULL;
+        struct Node* tail1 = NULL;
+        struct Node* tail2 = NULL;
+        struct Node* head2 = NULL;
+        int n1, n2, tmp, x;
+        cin >> n1;
+        while (n1--) {
+            cin >> tmp;
+            append(&head1, &tail1, tmp);
+        }
+        cin >> n2;
+        while (n2--) {
+            cin >> tmp;
+            append(&head2, &tail2, tmp);
+        }
+        cin >> x;
+        Solution obj;
+        cout << obj.countPairs(head1, head2, x) << '\n';
+    }
+    return 0;
+}
+// } Driver Code Ends
